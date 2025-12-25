6e372adc5cbbb90f9c8561dc810c8e2b638994a6
khangvo95 <khang.vo@getoutabox.com>
2025-12-25 19:43:01 +0700

    Fruit

--- src/AbstractInterface.java
+++ src/AbstractInterface.java
@@ -1,9 +1,11 @@
-package Animal;
-
 import Animal.Animal;
 import Animal.Chicken;
 import Animal.Tiger;
 import Edible.Edible;
+
+import Fruit.Fruit;
+import Fruit.Apple;
+import Fruit.Orange;
 
 public class AbstractInterface {
     public static void main(String[] args) {
@@ -17,6 +19,14 @@ public class AbstractInterface {
                 Edible edible = (Chicken) animal;
                 System.out.println(edible.howToEat());
             }
+        }
+
+        // Test package fruit
+        Fruit[] fruit = new Fruit[2];
+        fruit[0] = new Apple();
+        fruit[1] = new Orange();
+        for (Fruit fruit1 : fruit) {
+            System.out.println(fruit1.howToEat());
         }
     }
 }
